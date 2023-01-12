635f8b52b0d5081fbb4fa2e7921a2b6da3965c9a
yamaguchiyuya <yuya.soccer.0217.2000@gmail.com>
2023-01-12 22:46:06 +0900

    TODOコメント修正(練習2)

--- app/src/main/java/com/example/samplechatapplication/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/samplechatapplication/ui/main/MainViewModel.kt
@@ -3,5 +3,5 @@ package com.example.samplechatapplication.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: ViewModel実装予定
+    // TODO: ViewModelを実装予定
 }
